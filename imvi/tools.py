8fe093676f7b0a2f6908c07cfe43568b8f50b5b1
Jörg Neulist <21063011+joergneulist@users.noreply.github.com>
2025-12-28 21:38:54 +0100

    add destructor to clear up framebuffer on exit

--- imvi/tools.py
+++ imvi/tools.py
@@ -1,5 +1,6 @@
 from subprocess import Popen, run
 
+
 def find(mylist, value):
     try:
         return mylist.index(value)
@@ -23,6 +24,9 @@ class Executor:
     def __init__(self):
         self.file = None
         self.process = None
+
+    def __del__(self):
+        self.kill()
 
     def kill(self):
         if self.process is not None:
