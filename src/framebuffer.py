f4bd7c2f24513ff9b1f7203b0a941ed36e188c68
Joerg Neulist <joerg.neulist@appsphere.com>
2026-01-14 09:47:57 +0100

    correct color order and refactor

--- src/framebuffer.py
+++ src/framebuffer.py
@@ -2,6 +2,9 @@ import numpy as np
 from PIL import Image, ImageOps
 from sys import argv
 
+CONFIGPATH = '/sys/class/graphics/{}/'
+CURSORPATH = '/sys/class/graphics/fbcon/cursor'
+DEVICEPATH = '/dev/{}'
 
 def _read_config(filename):
     with open(filename, 'r') as fp:
@@ -14,15 +17,20 @@ class Framebuffer(object):
     # geometry 1920 1200 1920 1200 16
     # rgba 5/11,6/5,5/0,0/0
     def __init__(self, fbno=0):
-        self.dev = f'/dev/fb{fbno}'
-        config_dir = f'/sys/class/graphics/fb{fbno}/'
+        self.dev = f'fb{fbno}'
+        self.get_fb_config()
+        #self.disable_cursor() ONLY ROOT
+        self.map = np.memmap(DEVICEPATH.format(self.dev), dtype=np.uint16, mode='w+', shape=reversed(self.size))
+
+    def get_fb_config(self):
+        config_dir = CONFIGPATH.format(self.dev)
         self.size = tuple(_read_config(config_dir + 'virtual_size'))
         self.depth = _read_config(config_dir + 'bits_per_pixel')[0]
-        self.map = np.memmap(self.dev, dtype=np.uint16, mode='r+')
+        assert self.depth == 16, "Only 16bpp framebuffer is supported"
 
-    def __str__(self):
-        args = (self.dev, self.size, self.depth)
-        return '%s  size:%s  bits_per_pixel:%s' % args
+    def disable_cursor(self):
+        with open(CURSORPATH, 'w') as fp:
+            fp.write('0')
 
     def show(self, file_path):
         image = Image.open(file_path)
@@ -31,13 +39,11 @@ class Framebuffer(object):
         image = ImageOps.pad(image, (self.size[0], self.size[1]), color=(0, 0, 0))
         
         npimg = np.array(image, dtype=np.uint16)
-        print(npimg.shape)
         r, g, b = npimg[:, :, 0], npimg[:, :, 1], npimg[:, :, 2]
-        print(r.shape, g.shape, b.shape)
-        bgr565 = ((b >> 3) << 11) | ((g >> 2) << 5) | (r >> 3)
-        self.map[:] = bgr565.flatten()
+        self.map[:] = ((r >> 3) << 11) | ((g >> 2) << 5) | (b >> 3)
 
 
 if __name__ == '__main__':
     fb = Framebuffer()
     fb.show(argv[1])
+    input()
