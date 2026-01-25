379238347988df1858a71f7bb4145c5ed8da8712
Joerg Neulist <21063011+joergneulist@users.noreply.github.com>
2026-01-25 15:23:34 +0100

    tidy up event queue

--- src/eventqueue.py
+++ src/eventqueue.py
@@ -1,5 +1,7 @@
 
 from collections import deque
+from dataclasses import dataclass
+from enum import Enum
 from pathlib import Path
 from time import sleep, time
 
@@ -10,21 +12,21 @@ from usbmedia import USBMediaKeeper
 BTN_NEXT = 'next'
 BTN_PREV = 'prev'
 
-EVT_BUTTON = 'button'
-EVT_BUTTON_LONG = 'long'
-EVT_BUTTON_SHORT = 'short'
 
-EVT_MEDIA = 'media'
-EVT_MEDIA_LOAD = 'load'
-EVT_MEDIA_UNLOAD = 'unload'
+class Event(Enum):
+    Idle        =  0
+    ButtonShort = 11
+    ButtonLong  = 12
+    ImageLoad   = 21
+    ImageShow   = 22
+    MediaLoad   = 31
+    MediaUnload = 32
 
-EVT_IMAGE = 'image'
-EVT_IMAGE_TRIGGER = 'trigger'
-EVT_IMAGE_READY = 'ready'
 
-EVT_Q_EVT = 'evt'
-EVT_Q_TYPE = 'type'
-EVT_Q_PARAMS = 'params'
+@dataclass
+class EventEntry:
+    event: Event
+    params: tuple
 
 
 class EventQueue:
@@ -34,6 +36,7 @@ class EventQueue:
         self.verbose = verbose
         self.fileTracker = FileList()
         self.framebuffer = framebuffer
+        self.bored_since = time()
 
         # register triggers for media control
         try:
@@ -41,85 +44,91 @@ class EventQueue:
             self.btn_handlers = {}
             for button in [BTN_PREV, BTN_NEXT]:
                 self.btn_handlers[button] = ButtonHandler(self.config['pins'][button], button, self.cb_btn_short, self.cb_btn_long)
-                if self.verbose: print(self.btn_handlers[button])
+                if self.verbose:
+                    print(self.btn_handlers[button])
         except Exception:
             self.btn_handlers = None
             if self.verbose:
                 print('GPIO-Handlers failed!')
-    
-    def add_event(self, event, evtype, *evparams):
+
+
+    def process_idle_event(self, _):
+        # empty queue -> let's catch up on sleep!
+        sleep(0.1)
+
+        # debug mode: simulate button inputs
+        if self.btn_handlers is None and time() - self.bored_since > 10.0:
+            self.add_event(Event.ButtonShort, BTN_NEXT)                    
+            self.bored_since = time()
+
+
+    def process_button_short_event(self, name):
+        if name == BTN_NEXT:
+            self.fileTracker.next()
+        elif name == BTN_PREV:
+            self.fileTracker.prev()
+        self.add_event(Event.ImageLoad, self.fileTracker.get_file())
+
+
+    def process_button_long_event(self, name):
+        # TODO Implement different modes
+        self.process_button_short_event(name)
+
+
+    def process_image_load_event(self, path):
+        if path is not None:
+            buffer = self.framebuffer.load(path)
+            self.add_event(Event.ImageShow, buffer)
+
+
+    def process_image_show_event(self, buffer):
+        self.framebuffer.show(buffer)
+
+
+    def process_media_load_event(self, path):
+        self.fileTracker.load(path)
+        # TODO don't switch view if not necessary!
+        self.add_event(Event.ImageShow, self.fileTracker.get_file())
+
+
+    def process_media_show_event(self, path):
+        self.fileTracker.unload(path)
+        # TODO don't switch view if not necessary!
+        self.add_event(Event.ImageShow, self.fileTracker.get_file())
+
+
+    def add_event(self, event, *params):
         if self.verbose:
-            print(f'{time()} EVENTLOOP received event {event}: {evtype} with parameters {evparams}')
-        self.event_queue.append({EVT_Q_EVT: event, EVT_Q_TYPE: evtype, EVT_Q_PARAMS: evparams})
+            print(f'{time()} EVENTLOOP received event {event} with parameters {params}')
+        self.event_queue.append(EventEntry(event, *params))
 
 
     def get_event(self):
         if len(self.event_queue) == 0:
-            return None, None, None
+            return EventEntry(Event.Idle, ())
         evt = self.event_queue.popleft()
         if self.verbose:
-            print(f'{time()} EVENTLOOP process event {evt[EVT_Q_EVT]}: {evt[EVT_Q_TYPE]} with parameters {evt[EVT_Q_PARAMS]}')
-        return evt[EVT_Q_EVT], evt[EVT_Q_TYPE], evt[EVT_Q_PARAMS]
-
-
-    def update_view(self):
-        if self.verbose: print('update view')
-        self.fileTracker.view(self.framebuffer)
+            print(f'{time()} EVENTLOOP process event {evt.event} with parameters {evt.params}')
+        return evt
 
 
     def loop(self):
-        bored_since = time()
         while True:
-            event, evtype, evparams = self.get_event()
-            
-            if event is None:
-                # debug mode: simulate button inputs
-                if self.btn_handlers is None and time() - bored_since > 10.0:
-                    self.add_event(EVT_BUTTON, EVT_BUTTON_SHORT, BTN_NEXT)                    
-                    bored_since = time()
-                # empty queue -> let's catch up on sleep!
-                sleep(0.1)
-            
-            elif event == EVT_BUTTON:
-                if evparams[0] == BTN_NEXT:
-                    self.fileTracker.next()
-                elif evparams[0] == BTN_PREV:
-                    self.fileTracker.prev()
-                if file := self.fileTracker.get_file() is not None:
-                    self.add_event(EVT_IMAGE, EVT_IMAGE_TRIGGER, file)
-            
-            elif event == EVT_IMAGE:
-                if evtype == EVT_IMAGE_TRIGGER:
-                    if len(evparams) == 0:
-                        # TODO display waiting splash
-                        pass
-                    imgBitmap = self.framebuffer.prepare(evparams[0])
-                    self.add_event(EVT_IMAGE, EVT_IMAGE_READY, imgBitmap)
-                elif evtype == EVT_IMAGE_READY:
-                    # TODO add caching
-                    # TODO put image conversion in separate thread
-                    self.framebuffer.show(evparams[0])
-                else:
-                    raise NotImplementedError(f'illegal event {event}: {evtype}, {evparams}')
-            
-            elif event == EVT_MEDIA:
-                if evtype == EVT_MEDIA_LOAD:
-                    self.fileTracker.load(evparams[0])
-                    # TODO don't switch view if not necessary!
-                    self.add_event(EVT_IMAGE, EVT_IMAGE_TRIGGER, self.fileTracker.get_file())
-                elif evtype == EVT_MEDIA_UNLOAD:
-                    self.fileTracker.unload(evparams[0])
-                    # TODO don't switch view if not necessary!
-                    self.add_event(EVT_IMAGE, EVT_IMAGE_TRIGGER, self.fileTracker.get_file())
-                else:
-                    raise NotImplementedError(f'illegal event {event}: {evtype}, {evparams}')
+            EVT_HANDLERS = {
+                Event.Idle:        self.process_idle_event,
+                Event.ButtonShort: self.process_button_short_event,
+                Event.ButtonLong:  self.process_button_long_event,
+                Event.ImageLoad:   self.process_image_load_event,
+                Event.ImageShow:   self.process_image_show_event,
+                Event.MediaLoad:   self.process_media_load_event,
+                Event.MediaUnload: self.process_media_show_event
+            }
+            next_task = self.get_event()
+            EVT_HANDLERS[next_task.event](next_task.params)
 
-            else:
-                raise NotImplementedError(f'illegal event {event}: {evtype}, {evparams}')
-        
 
-    def cb_btn_long(self, name):     self.add_event(EVT_BUTTON, EVT_BUTTON_LONG,  name)
-    def cb_btn_short(self, name):    self.add_event(EVT_BUTTON, EVT_BUTTON_SHORT, name)
-    def cb_dev_mounted(self, path):  self.add_event(EVT_MEDIA,  EVT_MEDIA_LOAD,   path)
-    def cb_dev_unmounted(self, path):self.add_event(EVT_MEDIA,  EVT_MEDIA_UNLOAD, path)
+    def cb_btn_long(self, name):     self.add_event(Event.ButtonLong,  name)
+    def cb_btn_short(self, name):    self.add_event(Event.ButtonShort, name)
+    def cb_dev_mounted(self, path):  self.add_event(Event.MediaLoad,   path)
+    def cb_dev_unmounted(self, path):self.add_event(Event.MediaUnload, path)
 
