80ef478c9bc7dedcad7fbf27f68b91f3d4ea2a6e
Joerg Neulist <21063011+joergneulist@users.noreply.github.com>
2026-01-26 14:05:38 +0100

    Changes: * disable_splash fix * boot with dmesg on tty10

---
+++ buildroot_tailoring/board/post-build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -u
+set -e
+
+# Add a console on tty1
+if [ -e ${TARGET_DIR}/etc/inittab ]; then
+    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
+	sed -i '/GENERIC_SERIAL/a\
+tty1::respawn:/usr/bin/python3 '$2'/imvi.py\
+tty2::respawn:/sbin/getty -L tty2 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
+# systemd doesn't use /etc/inittab, enable getty.tty1.service instead
+elif [ -d ${TARGET_DIR}/etc/systemd ]; then
+    mkdir -p "${TARGET_DIR}/etc/systemd/system/getty.target.wants"
+    ln -sf /lib/systemd/system/getty@.service \
+       "${TARGET_DIR}/etc/systemd/system/getty.target.wants/getty@tty1.service"
+fi
